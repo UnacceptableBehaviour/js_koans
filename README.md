19cf200a7a906a12398e8730c6cc6c9c4567ff68
Simon <simon@ferdiesfoodlab.co.uk>
2019-10-19 14:23:52 +0100

    1st unit test and some notes, QUnit API

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # js_koans
 ## Getting up to speed on javascript  
 
-### Koans from here: https://github.com/liammclennan/JavaScript-Koans  
-Qunit Ref: http://qunitjs.com/
-Setup: clone repo
-Open jskoans.htm 
+### Koans from here: https://github.com/liammclennan/JavaScript-Koans    
+Qunit Ref: http://qunitjs.com/  
+Setup: clone repo  
+QUnit Assertions & other: https://api.qunitjs.com/  
+Install: npm install -g qunit (for CLI access)
 
+Open jskoans.htm, proceed to fix test in file indicated by the failed test! 
  
--- topics/about_asserts.js
+++ topics/about_asserts.js
@@ -1,14 +1,21 @@
 
 module("About Asserts (topics/about_asserts.js)");
 
+//     /------ test name for report 
 test("ok", function() {
-    ok(__ === true, 'what will satisfy the ok assertion?');
+    ok(true === true, 'what will satisfy the ok assertion?');
 });
 
-test("not ok", function() {
-    ok(__ === false, 'what is a false value?');
+//         /------ test name for report - PASSES - can be anything!
+test("fliberty jibbert", function() {
+//test("not ok", function() {    
+    ok(false === false, 'what is a false value?');
 });
+
 
 test("equal", function() {
-    equal(__, 1 + 1, 'what will satisfy the equal assertion?');
-});
+    equal(2, 1 + 1, 'what will satisfy the equal assertion?');
+});  //\\
+    //  \\
+   //    tests here: https://api.qunitjs.com/
+  // 
