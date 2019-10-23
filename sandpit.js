c743c67dd732380830b69f0c36b90388ce27f734
Simon <simon@ferdiesfoodlab.co.uk>
2019-10-23 11:11:33 +0100

    added buttons and tagert calls

--- sandpit.js
+++ sandpit.js
@@ -1,4 +1,31 @@
 // sandpit
 console.log("\n*\n*\n*\nSandpit LOADED\n*\n*\n*\n");
 
+var but_f1 = document.getElementById('b_f1');
+var but_f2 = document.getElementById('b_f2');
+var but_f3 = document.getElementById('b_f3');
+var but_f4 = document.getElementById('b_f4');
 
+//
+//
+but_f1.addEventListener('click', function(event) {
+  console.log('\n*\n*\n*\nButton F1 clicked');
+});
+
+//
+//
+but_f2.addEventListener('click', function(event) {
+  console.log('\n*\n*\n*\nButton F2 clicked');
+});
+
+//
+//
+but_f3.addEventListener('click', function(event) {
+  console.log('\n*\n*\n*\nButton F3 clicked');
+});
+
+//
+//
+but_f4.addEventListener('click', function(event) {
+  console.log('\n*\n*\n*\nButton F4 clicked');
+});
